ba5fff982d3278cb06c938f70ed20bdf7feee535
Kali <jazz.sumariva@gmail.com>
2022-06-07 11:59:55 -0400

    working

--- docker_lamp/html2/index.php
+++
@@ -1,9 +0,0 @@
-<?php
-include_once "header.php";
-?>
-
-<h1 class="text-center" style="padding: 200px">You are logged in as user <?php echo $_SESSION['userid']; ?>!</h1>
-
-<?php
-include_once "footer.php";
-?>
--- docker_lamp/html2/logout.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-include "auth_check.php";
-
-session_destroy();
-
-echo "Logout successful";
-header("Location: ./login.php");
